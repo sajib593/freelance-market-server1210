75aa3fcc5a857a25d89d2d8b0b50170ca32b68ff
sajib593 <sajib.hoossain11@gmail.com>
2025-11-10 01:49:30 -0800

    all jobs done

--- index.js
+++ index.js
@@ -1,14 +1,101 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.send('Hell')
 })
 
 
+app.use(cors());
+app.use(express.json())
+
+// freelance-market
+// qi00OJCNpTTcHItW
 
 
+
+
+const uri = "mongodb+srv://freelance-market:qi00OJCNpTTcHItW@cluster0.vkkq9zu.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    let db = client.db('ferrlanceMarket');
+    let allJobsCollection = db.collection('allJobs');
+
+
+    app.get('/allJobs', async(req, res)=>{
+
+      let allJobs = await allJobsCollection.find().toArray();
+      // console.log(allJobs);
+      res.send(allJobs)
+
+    })
+
+
+    // add jobs api
+
+  
+
+
+    // fins and show viewDetails 
+
+    
+
+        app.get('/allJobs/:id', async(req, res)=>{
+          let id = req.params.id;
+          // console.log(id);
+          let query = {_id : new ObjectId(id)}
+          let result = await allJobsCollection.findOne(query);
+          res.send(result)
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 

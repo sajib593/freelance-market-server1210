d03f879cce43133216595c6dd3452ce1373acef2
sajib593 <sajib.hoossain11@gmail.com>
2025-11-12 21:25:26 -0800

    before deploy

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     let db = client.db('ferrlanceMarket');
